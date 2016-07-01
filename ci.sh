eb3e668847a0b8d9dd7ea2e0377f57c55a62567c
AutomatedTester <david.burns@theautomatedtester.co.uk>
2016-07-01 22:55:47 +0100

    Add Win32 binaries to the releases

--- ci.sh
+++ ci.sh
@@ -12,31 +12,34 @@ rustup_target_add() {
 # Configure rustc target for cross compilation.  Provided with a build
 # target, this will determine which linker to use for cross compilation.
 cargo_config() {
-    local prefix
+	local prefix
 
-    case "$TARGET" in
-    aarch64-unknown-linux-gnu)
-        prefix=aarch64-linux-gnu
-        ;;
-    arm*-unknown-linux-gnueabihf)
-        prefix=arm-linux-gnueabihf
-        ;;
-    arm-unknown-linux-gnueabi)
-        prefix=arm-linux-gnueabi
-        ;;
-    mipsel-unknown-linux-musl)
-        prefix=mipsel-openwrt-linux
-        ;;
-    x86_64-pc-windows-gnu)
-        prefix=x86_64-w64-mingw32
-        ;;
-    *)
-        return
-        ;;
-    esac
+	case "$TARGET" in
+	aarch64-unknown-linux-gnu)
+		prefix=aarch64-linux-gnu
+		;;
+	arm*-unknown-linux-gnueabihf)
+		prefix=arm-linux-gnueabihf
+		;;
+	arm-unknown-linux-gnueabi)
+		prefix=arm-linux-gnueabi
+		;;
+	mipsel-unknown-linux-musl)
+		prefix=mipsel-openwrt-linux
+		;;
+	i686-pc-windows-gnu)
+		prefix=i686-w64-mingw32
+		;;
+	x86_64-pc-windows-gnu)
+		prefix=x86_64-w64-mingw32
+		;;
+	*)
+		return
+		;;
+	esac
 
-    mkdir -p ~/.cargo
-    cat >>~/.cargo/config <<EOF
+	mkdir -p ~/.cargo
+	cat >>~/.cargo/config <<EOF
 [target.$TARGET]
 linker = "$prefix-gcc"
 EOF
