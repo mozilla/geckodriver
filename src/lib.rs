faeb63a7757e5ec4a62a1622ef0a6f596b39cdd1
Andreas Tolfsen <ato@mozilla.com>
2016-09-08 19:41:30 +0100

    replace log with slog
    The default log library has the disadvantage that it cannot be
    reinitialised.  geckodriver needs the ability to set up the logger
    again with a log level passed in with the `firefoxOptions.log.level`
    capability when a new session request is made.
    
    slog redirects calls to the log crate's `info!()`, `debug!()`, `trace!()`
    &c. macros to a drain that we reinitialise when `logging::init(level:
    LogLevel)` is called.
    
    As this patch removes the env-logger crate, support for setting the
    `RUST_LOG` environment variable is removed with this patch.  The correct
    ways to request a log level is to use the `-vv` or `--log LEVEL` flags, or
    the `firefoxOptions.log.level` capability as described in the README.md.
    
    This patch also fixes a bug where the capability log level would
    override the log level requested by flag in `MarionetteSettings`, causing
    subsequent WebDriver sessions to inherit the log level.  For example,
    if session 1 used `firefoxOptions.log.level` and session 2 did not,
    it would reuse the level from before because `MarionetteSettings` would
    not be reset.

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+extern crate chrono;
+#[macro_use]
+extern crate lazy_static;
+extern crate hyper;
+extern crate mozprofile;
+extern crate mozrunner;
+extern crate regex;
+extern crate rustc_serialize;
+#[macro_use]
+extern crate slog;
+extern crate slog_atomic;
+extern crate slog_stdlog;
+extern crate slog_stream;
+extern crate zip;
+extern crate webdriver;
+
+#[macro_use]
+extern crate log;
+
+pub mod logging;
+
+macro_rules! try_opt {
+    ($expr:expr, $err_type:expr, $err_msg:expr) => ({
+        match $expr {
+            Some(x) => x,
+            None => return Err(WebDriverError::new($err_type, $err_msg))
+        }
+    })
+}
+
+// TODO(ato): Split marionette.rs up further
+mod marionette;
+pub use marionette::{extension_routes, FirefoxOptions, MarionetteHandler, MarionetteSettings};
