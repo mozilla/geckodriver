4d2d63a94173929e3dbd77d4c110d1e35bf31d00
Andreas Tolfsen <ato@mozilla.com>
2016-11-11 00:20:24 +0000

    main: use ? operator and lint

--- src/main.rs
+++ src/main.rs
@@ -55,65 +55,68 @@ fn app<'a, 'b>() -> App<'a, 'b> {
     App::new(format!("geckodriver {}", crate_version!()))
         .about("WebDriver implementation for Firefox.")
         .arg(Arg::with_name("webdriver_host")
-             .long("host")
-             .value_name("HOST")
-             .help("Host ip to use for WebDriver server (default: 127.0.0.1)")
-             .takes_value(true))
+            .long("host")
+            .value_name("HOST")
+            .help("Host ip to use for WebDriver server (default: 127.0.0.1)")
+            .takes_value(true))
         .arg(Arg::with_name("webdriver_port_alias")
-             .long("--webdriver-port")
-             .takes_value(true)
-             .hidden(true))
+            .long("--webdriver-port")
+            .takes_value(true)
+            .hidden(true))
         .arg(Arg::with_name("webdriver_port")
-             .short("p")
-             .long("port")
-             .value_name("PORT")
-             .help("Port to use for WebDriver server (default: 4444)")
-             .takes_value(true)
-             .conflicts_with("webdriver_port_alias"))
+            .short("p")
+            .long("port")
+            .value_name("PORT")
+            .help("Port to use for WebDriver server (default: 4444)")
+            .takes_value(true)
+            .conflicts_with("webdriver_port_alias"))
         .arg(Arg::with_name("binary")
-             .short("b")
-             .long("binary")
-             .value_name("BINARY")
-             .help("Path to the Firefox binary")
-             .takes_value(true))
+            .short("b")
+            .long("binary")
+            .value_name("BINARY")
+            .help("Path to the Firefox binary")
+            .takes_value(true))
         .arg(Arg::with_name("marionette_port")
-             .long("marionette-port")
-             .value_name("PORT")
-             .help("Port to use to connect to Gecko (default: random free port)")
-             .takes_value(true))
+            .long("marionette-port")
+            .value_name("PORT")
+            .help("Port to use to connect to Gecko (default: random free port)")
+            .takes_value(true))
         .arg(Arg::with_name("connect_existing")
-             .long("connect-existing")
-             .requires("marionette_port")
-             .help("Connect to an existing Firefox instance"))
+            .long("connect-existing")
+            .requires("marionette_port")
+            .help("Connect to an existing Firefox instance"))
         .arg(Arg::with_name("verbosity")
-             .short("v")
-             .multiple(true)
-             .conflicts_with("log_level")
-             .help("Log level verbosity (-v for debug and -vv for trace level)"))
+            .short("v")
+            .multiple(true)
+            .conflicts_with("log_level")
+            .help("Log level verbosity (-v for debug and -vv for trace level)"))
         .arg(Arg::with_name("log_level")
-             .long("log")
-             .takes_value(true)
-             .value_name("LEVEL")
-             .possible_values(
-                 &["fatal", "error", "warn", "info", "config", "debug", "trace"])
-             .help("Set Gecko log level"))
-         .arg(Arg::with_name("version")
-             .short("V")
-             .long("version")
-             .help("Prints version and copying information"))
+            .long("log")
+            .takes_value(true)
+            .value_name("LEVEL")
+            .possible_values(&["fatal", "error", "warn", "info", "config", "debug", "trace"])
+            .help("Set Gecko log level"))
+        .arg(Arg::with_name("version")
+            .short("V")
+            .long("version")
+            .help("Prints version and copying information"))
 }
 
 fn run() -> ProgramResult {
     let matches = app().get_matches();
 
     if matches.is_present("version") {
-        println!("geckodriver {}\n\n{}", crate_version!(),
-"The source code of this program is available at
-https://github.com/mozilla/geckodriver.
+        println!("geckodriver {}\n\n{}",
+                 crate_version!(),
+                 "The source code of this program is available at
+\
+                  https://github.com/mozilla/geckodriver.
 
-This program is subject to the terms of the Mozilla Public License 2.0.
-You can obtain a copy of the license at https://mozilla.org/MPL/2.0/.");
-        return Ok(())
+This program is subject to the terms \
+                  of the Mozilla Public License 2.0.
+You can obtain a copy of the license at \
+                  https://mozilla.org/MPL/2.0/.");
+        return Ok(());
     }
 
     let host = matches.value_of("webdriver_host").unwrap_or("127.0.0.1");
@@ -131,11 +134,13 @@ You can obtain a copy of the license at https://mozilla.org/MPL/2.0/.");
     let binary = matches.value_of("binary").map(|x| PathBuf::from(x));
 
     let marionette_port = match matches.value_of("marionette_port") {
-        Some(x) => match u16::from_str(x) {
-            Ok(x) => Some(x),
-            Err(_) => return Err((ExitCode::Usage, "invalid Marionette port".to_owned())),
-        },
-        None => None
+        Some(x) => {
+            match u16::from_str(x) {
+                Ok(x) => Some(x),
+                Err(_) => return Err((ExitCode::Usage, "invalid Marionette port".to_owned())),
+            }
+        }
+        None => None,
     };
 
     // overrides defaults in Gecko
@@ -160,8 +165,8 @@ You can obtain a copy of the license at https://mozilla.org/MPL/2.0/.");
     };
 
     let handler = MarionetteHandler::new(settings);
-    let listening = try!(webdriver::server::start(addr, handler, extension_routes())
-        .map_err(|err| (ExitCode::Usage, err.to_string())));
+    let listening = webdriver::server::start(addr, handler, extension_routes())
+        .map_err(|err| (ExitCode::Usage, err.to_string()))?;
     info!("Listening on {}", listening.socket);
     Ok(())
 }
@@ -172,7 +177,7 @@ fn main() {
         Err((exit_code, reason)) => {
             error!("{}", reason);
             exit_code
-        },
+        }
     };
 
     std::io::stdout().flush().unwrap();
